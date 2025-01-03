52768ccf802a3865892957b497f328b280eb9974
avalchev8 <atanasvalchev@gmail.com>
2025-01-03 13:15:24 +0200

    Fix LP initialization

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// Track Event
-document.getElementById('eventBtn').addEventListener('click', function() {
-    leanplum.track('Button Clicked');
+// Ensure leanplum is defined
+ // Initialize Leanplum
+ Leanplum.setAppIdForProductionMode('app_y2XiC7zWyC7Q89pm6Rx07YI6sz5N91WmkQeTJBBqDjI', 'prod_lIS9NZFo0N6rMhi0laGeiLQnV2i8gbekmBmY6gPN0o4'); 
+
+ // Start Leanplum when the page loads
+ Leanplum.start(function(success) {
+  console.log('Success: ' + success);
+  console.log('Variables', Leanplum.getVariables());
+ });
+if (typeof Leanplum !== 'undefined') {
+  // Track Event
+  document.getElementById('eventBtn').addEventListener('click', function() {
+    Leanplum.track('Button Clicked');
   });
-  
+
   // Set User Attribute
   document.getElementById('attrBtn').addEventListener('click', function() {
-    leanplum.setUserAttributes({
+    Leanplum.setUserAttributes({
       'username': 'testuser',
       'age': 30 
     });
   });
+} else {
+  console.error('leanplum is not defined');
+}
